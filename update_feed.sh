0eff1a5b770a856d5ab096191f7459aa6f120a10
Mark Haylock <mark@resolvedigital.co.nz>
2013-08-13 17:05:47 +1200

    Package javascript enabled version to point to javascript_enabled branch of mhaylock repo.

--- update_feed.sh
+++ update_feed.sh
@@ -3,7 +3,7 @@ tar --exclude='.DS_Store' -czf feed/Rails2.3.tgz Rails2.3.docset
 cat > feed/Rails2.3.xml <<FEED_XML
 <entry>
     <version>$version</version>
-    <url>https://raw.github.com/jipumarino/rails-2.3-docset/master/feed/Rails2.3.tgz</url>
+    <url>https://raw.github.com/mhaylock/rails-2.3-docset/javascript_enabled/feed/Rails2.3.tgz</url>
 </entry>
 FEED_XML
 
